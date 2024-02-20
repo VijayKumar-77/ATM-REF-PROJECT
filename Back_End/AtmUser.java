275f08b0091982f7ea2e4f304ed5136af89afbec
THE_VIJAY <vijaykumarmetoo@gmail.com>
2024-02-20 16:41:53 +0530

    user updated

---
+++ Back_End/AtmUser.java
@@ -0,0 +1,40 @@
+
+public class AtmUser {
+	public int Pin;
+	int AtmPin;
+	int AvailBalance;
+	int Deposit ;
+	int Withdraw ;
+	public int getPin() {
+		return Pin;
+	}
+	public void setPin(int pin) {
+		Pin = pin;
+	}
+	public int getAtmPin() {
+		return AtmPin;
+	}
+	public void setAtmPin(int atmPin) {
+		AtmPin = atmPin;
+	}
+	public int getAvailBalance() {
+		return AvailBalance;
+	}
+	public void setAvailBalance(int availBalance) {
+		AvailBalance = availBalance;
+	}
+	public int getDeposit() {
+		return Deposit;
+	}
+	public void setDeposit(int deposit) {
+		Deposit = deposit;
+	}
+	public int getWithdraw() {
+		return Withdraw;
+	}
+	public void setWithdraw(int withdraw) {
+		Withdraw = withdraw;
+	}
+	
+	
+}
